794405218be2b3b6eae91567311c4c2526cedf0b
rahmed <rahmed@student.42nice.fr>
2023-05-20 21:27:17 +0200

    update front / arborescence updated with login on first page

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,43 +1,43 @@
-<!-- Home  "/" page content -->
+<!-- Login page content -->
 
 <script lang="ts">
-	import pongAnim from '$lib/images/pongAnim.png';
+	async function redir_42() {
+		window.location.href =
+			'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-2f818a332a9a2006b00174a76ce71efe9e374cc942b996040f79806509ce968d&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fhome&response_type=code';
+	}
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte pong app" />
+	<title>Login</title>
+	<meta name="description" content="Login Page" />
 </svelte:head>
 
-<section>
-	<h1>
-		Welcome to<br /><strong>Ft_Trancendance</strong><br /><br />
-		<span class="pongAnim">
-			<picture>
-				<img src={pongAnim} alt="pongAnim" style="height: 300px; width: auto;" />
-			</picture>
-		</span>
-	</h1>
-</section>
+<main>
+	<div class="center">
+		<div class="text-column">
+			<h1>Please LOGIN <br /> with your intra42</h1> <br /> <br />
+			<button on:click={redir_42}>LOGIN</button> <br /> <br />
+		</div>
+	</div>
+</main>
 
 <style>
-	section {
+	.center {
 		display: flex;
-		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 0.6;
 	}
 
-	h1 {
-		width: 100%;
+	button {
+		background-color: #007fff;
+		color: #fff;
+		font-size: 1.2rem;
+		padding: 1rem 2rem;
+		border: none;
+		border-radius: 15px;
+		cursor: pointer;
 	}
-
-	.pongAnim img {
-		position: static;
-		/* width: 100%;
-		height: 100%; */
-		top: 1;
-		display: inside;
+	button:hover {
+		background-color: #0055ff;
 	}
 </style>
