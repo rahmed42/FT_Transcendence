e579a19387caf39bb2960ec453a15c5bc26ef051
rahmed <rahmed@student.42nice.fr>
2023-05-27 16:32:16 +0200

    commit before rooms backend

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -8,15 +8,18 @@ export class UserService {
     constructor(private prisma: PrismaService, private jwt: JwtService) {}
         async getInfo(tokenObject: { jwt: string }) {
             const decode = this.jwt.decode(tokenObject.jwt);
-            if (typeof decode === 'object')
-            {
-                const user = await this.prisma.user.findUnique({
-                    where: {
-                        id: decode.id,
-                    },
-                })
-                console.log(user);
-                return user;
-            }
+			console.log(decode);
+            // if (typeof decode === 'object')
+            // {
+            //     const user = await this.prisma.user.findUnique({
+            //         where: {
+            //             id: decode.id,
+            //         },
+            //     })
+            //     console.log(user);
+            //     return user;
+            // }
+			return decode; // replaced by decode car bloque le login a cause de .id
+
         }
 }
