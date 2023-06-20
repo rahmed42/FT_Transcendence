c4b78e6070b1ab72a3ce53be870a414e9e8c8325
ddupont06 <dorian.dupont@hotmail.fr>
2023-06-20 13:54:34 +0200

    improved_design_user_profile

--- Frontend/src/routes/profile/info/+page.svelte
+++ Frontend/src/routes/profile/info/+page.svelte
@@ -2,12 +2,20 @@
 	import { get, writable } from 'svelte/store';
 	import { setUser, user } from '../../../stores/user';
 	import { onMount } from 'svelte';
+    import winIcon from '../../../lib/images/win-icon.png';
+    import lossIcon from '../../../lib/images/loss-icon.png';
+    import ladderIcon from '../../../lib/images/ladder-icon.png';
 
     const friend = writable({});  // Local state for friend
     let stats = null;
     let matchHistory = [];
 
     const friend_username = new URLSearchParams(window.location.search).get('login');
+
+    function formatDate(isoDateString) {
+        const date = new Date(isoDateString);
+        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    }
 
 	onMount(async () => {
 		async function getUserInfo() {
@@ -39,55 +47,140 @@
 	<meta name="description" content="User profile" />
 </svelte:head>
  
-<h1 class="title"><strong>{$friend.login}</strong>'s Profil Page</h1>
-
-{#if $friend.avatar}
-    <img class="pp" id="avatar" src={$friend.avatar} alt="avatar"/>
-{:else}
-    <img class="pp" id="avatar" src={$friend.large_pic} alt={`Picture of ${$friend.login}`}/>
-{/if}
-
-<h2 class="section-heading">Stats</h2>
-{#if stats}
-    <ul>
-        <li>Wins: {stats.wins}</li>
-        <li>Losses: {stats.losses}</li>
-        <li>Ladder Level: {stats.ladderLevel}</li>
-    </ul>
-{:else}
-    <p>No stats available</p>
-{/if}
-
-<h2 class="section-heading">Match History</h2>
-{#if matchHistory.length > 0}
-    <ul>
-        {#each matchHistory as match (match.id)}
-            <li>Game Type: {match.gameType} | Score: {match.myScore} - {match.opponentScore} ({match.opponentName}) | Result: {match.result} | Date: {match.timestamp}</li>
-        {/each}
-    </ul>
-{:else}
-    <p>No match history available</p>
-{/if}
+<div class="profile">
+    <h1 class="title"><span class="username"><strong>{$friend.login}</strong></span>'s Profile Page</h1>
+    
+    {#if $friend.avatar}
+        <img class="pp" id="avatar" src={$friend.avatar} alt="avatar"/>
+    {:else}
+        <img class="pp" id="avatar" src={$friend.large_pic} alt={`Picture of ${$friend.login}`}/>
+    {/if}
+    <hr class="section-divider" />
+    <h2 class="section-heading">Stats</h2>
+    <div class="stats-card">
+        {#if stats}
+            <div class="stat-item">
+                <img src={winIcon} alt="Wins Icon">
+                <h3>{stats.wins}</h3>
+                <p>Wins</p>
+            </div>
+            <div class="stat-item">
+                <img src={lossIcon} alt="Losses Icon">
+                <h3>{stats.losses}</h3>
+                <p>Losses</p>
+            </div>
+            <div class="stat-item">
+                <img src={ladderIcon} alt="Ladder Icon">
+                <h3>{stats.ladderLevel}</h3>
+                <p>Ladder Level</p>
+            </div>
+        {:else}
+            <p>No stats available</p>
+        {/if}
+    </div>
+    <hr class="section-divider" />
+    <h2 class="section-heading">Match History</h2>
+    {#if matchHistory.length > 0}
+        <table class="match-history-table">
+            <thead>
+                <tr>
+                    <th>Game Type</th>
+                    <th>Score</th>
+                    <th>Result</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each matchHistory as match (match.id)}
+                    <tr>
+                        <td>{match.gameType}</td>
+                        <td>{match.myScore} - {match.opponentScore} (vs. {match.opponentName})</td>
+                        <td>{match.result}</td>
+                        <td>{formatDate(match.timestamp)}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    {:else}
+        <p>No match history available</p>
+    {/if}
+</div>
 
 <style>
+    .section-divider {
+        border: 0;
+        border-top: 1px solid white;
+        margin: 2em 0;
+    }
+    .profile {
+        width: 80%;
+        margin: 0 auto;
+        padding: 20px;
+        background: rgba(0, 0, 0, 0.6);
+        border-radius: 15px;
+        color: #fff;
+    }
 	.section-heading {
 		font-weight: bold;
-		font-size: 20px;
+		font-size: 24px;
+        color: #ddd;
 	}
     .pp {
         display: block;
-        margin-left: auto;
-        margin-right: auto;
-        border-radius: 0 100px 0 100px;
-        width: 450px;
+        margin: 0 auto 20px;
+        border-radius: 50%;
+        width: 150px;
+        height: 150px;
+        object-fit: cover;
         border: 2px solid rgb(88, 44, 231);
-        padding: 5px;
-        max-width: 250px;
-        max-height: 250px;
     }
-	.title {
-		color: rgb(212, 203, 29);
-		margin-left: center;
-		margin-right: center;
+    .title {
+		color: #eeeeee;
+		text-align: center;
 	}
+    .username {
+        color: rgb(212, 203, 29);
+    }
+    .stats-card {
+        display: flex;
+        justify-content: space-around;
+    }
+    .stat-item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+    .stat-item img {
+        width: 40px;
+        height: 40px;
+    }
+    .stat-item h3, .stat-item p {
+        margin: 0;  /* Remove margins to prevent alignment issues */
+    }
+    .match-history-table {
+        width: 100%;
+        border-collapse: collapse;
+        color: #eee;
+    }
+    .match-history-table th,
+    .match-history-table td {
+        border: 1px solid #ddd;
+        padding: 8px;
+        text-align: center;
+    }
+    .match-history-table tr:nth-child(even) {
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+    .match-history-table tr:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+    .match-history-table th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: center;
+        background-color: rgba(0, 0, 0, 0.8);
+        color: #fff;
+    }
 </style>
